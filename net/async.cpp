07ab725596b6aea20967361781776b04da0faf15
phomespace <44018082+phomespace@users.noreply.github.com>
2018-11-30 17:45:25 +0800

    删除了all.h文件  增加了async.cpp文件

---
+++ net/async.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "./include/uv.h"
+
+using namespace std;
+
+
+void async_cd(uv_async_t * handle, int status)
+{
+	cout << "my thread id:" << uv_thread_self() << endl;
+
+}
+
+int main()
+{
+    uv_loop_t * loop ;
+
+	loop = uv_default_loop();
+	
+    cout << "main thread id:" << uv_thread_self() << endl;
+
+	uv_async_t async ;
+	
+    uv_async_init(loop,&async,(uv_async_cb) async_cd);
+
+	uv_async_send(&async);
+
+	uv_run(loop,UV_RUN_DEFAULT);
+}
