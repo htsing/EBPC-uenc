44811ade1c05af462c23a5b5c38ea6bdbeea1e0a
phomespace <44018082+phomespace@users.noreply.github.com>
2018-12-21 13:21:00 +0800

    添加了PjPool.cpp文件

---
+++ net/PjPool.cpp
@@ -0,0 +1,40 @@
+#include "PjPool.h"
+#include "PjGlobal.h"
+
+#define DEFAULT_POOL_NAME   "pjpool"
+#define DEFAULT_INITIAL_SIZE    1024
+#define DEFAULT_INCREMENT_SIZE  1024
+
+CPjPool::CPjPool(){
+    mpPjPool = nullptr;
+    mstrPoolName = DEFAULT_POOL_NAME;
+    miInitialSize = DEFAULT_INITIAL_SIZE;
+    miIncrementSize = DEFAULT_INCREMENT_SIZE;
+    InitPool();
+}
+
+CPjPool::~CPjPool(){
+    if (nullptr != mpPjPool){
+        pj_pool_release(mpPjPool);
+        mpPjPool = nullptr;
+    }
+}
+
+CPjPool::CPjPool(std::string strPoolName,  int iInitialSize, int iIncrementSize) {
+    miInitialSize = iInitialSize;
+    miIncrementSize = iIncrementSize;
+    mstrPoolName = strPoolName;
+    InitPool();
+}
+
+int CPjPool::InitPool() {
+    mpPjPool = pj_pool_create(sPjGlobal->GetGlobalPool()->factory, mstrPoolName.c_str(), miInitialSize, miIncrementSize, nullptr);
+    ASSERT_RET_VALUE(nullptr != mpPjPool, 1);
+
+    return 0;
+}
+
+void* CPjPool::Alloc(pj_size_t iSize) {
+    ASSERT_RET_VALUE(nullptr != mpPjPool && iSize > 0, nullptr);
+    return pj_pool_zalloc(mpPjPool, iSize);
+}
