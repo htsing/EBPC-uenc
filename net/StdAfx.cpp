05921e787e414d4bfbaf6a942fa2a32b04e11dea
phomespace <44018082+phomespace@users.noreply.github.com>
2019-02-26 08:32:26 +0800

    增加了StdAfx.cpp文件

---
+++ net/StdAfx.cpp
@@ -0,0 +1,24 @@
+// stdafx.cpp : source file that includes just the standard includes
+//	ServerIOCP.pch will be the pre-compiled header
+//	stdafx.obj will contain the pre-compiled type information
+
+#include <iostream>
+
+#include "stdafx.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "i love you" << endl;
+	
+}
+
+
+
+
+
+
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
